31d96a3a8f21c564edd29ebad74dd36de4fcb8c1
catozzi <andreacatozzi@lce12432.corp.capgemini.com>
2015-10-19 16:59:17 +0200

    First commit
    Signed-off-by: catozzi <andreacatozzi@lce12432.corp.capgemini.com>

---
+++ FibaroHomeKitTool/FibaroSetupViewController.swift
@@ -0,0 +1,117 @@
+//
+//  FibaroSetupViewController.swift
+//  FibaroHomeKitTool
+//
+//  Created by Andrea Catozzi on 18/10/15.
+//  Copyright © 2015 Apple, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FibaroSetupViewController: UITableViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var fibaroHost: UITextField!
+    @IBOutlet weak var fibaroUserName: UITextField!
+    @IBOutlet weak var fibaroPassword: UITextField!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.updateHomeStoreFibaroCredentials()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func updateHomeStoreFibaroCredentials() {
+        HomeStore.sharedStore.fibaroHost = fibaroHost.text
+        HomeStore.sharedStore.fibaroUserName = fibaroUserName.text
+        HomeStore.sharedStore.fibaroPassword = fibaroPassword.text
+    
+    }
+    // MARK: - Table view data source
+
+/*    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        switch section {
+            case 0:
+                return 1
+            case 1:
+                return 1
+            case 2:
+                return 1
+            default:
+                return 0
+        }
+    }
+*/
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
